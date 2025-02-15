1863a4f0c4b43e8eb12cf8a4fa6c4bd42f19f6b0
Shan-N <shantanu.neve2000@gmail.com>
2025-02-15 20:39:33 +0530

    check?

--- src/app/compliance/cancel/page.tsx
+++ src/app/compliance/cancel/page.tsx
@@ -4,7 +4,7 @@ export const Cancel = () => {
     return (
         <main>
             <section>
-                
+                <h2>Cancel Order</h2>
             </section>
         </main>
     );
