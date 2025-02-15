97c8c96fc50e04d4722d29f00d8268d65ca8e937
Shan-N <shantanu.neve2000@gmail.com>
2025-02-15 20:52:51 +0530

    pages fix

---
+++ src/app/compliance/contact/page.tsx
@@ -0,0 +1,9 @@
+export default function Contact() {
+    return (
+        <main>
+            <section>
+                <h2>Cancel Order</h2>
+            </section>
+        </main>
+    );
+}
---
+++ src/app/compliance/privacy/page.tsx
@@ -0,0 +1,9 @@
+export default function Privacy() {
+    return (
+        <main>
+            <section>
+                <h2>Cancel Order</h2>
+            </section>
+        </main>
+    );
+}
