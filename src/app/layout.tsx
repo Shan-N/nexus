bb72816fe349913c73b99ce405662b7212a890a8
Shan-N <shantanu.neve2000@gmail.com>
2025-02-21 21:53:33 +0530

    vercel analytics🙏

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from '@vercel/analytics/next';
 import "./globals.css";
 
 
@@ -20,6 +21,7 @@ export default function RootLayout({
         className={`antialiased text-white bg-[#050020] h-screen`}
       >
         {children}
+        <Analytics />
       </body>
     </html>
     </>
